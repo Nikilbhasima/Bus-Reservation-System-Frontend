744cd24122a4b8d71451ce23190a3495b51ed18d
Saliv Maharjan <vermion00@gmail.com>
2025-08-29 20:46:48 +0545

    App Design

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+ import './App.css';
+ 
 const App = () => {
   return (
     <div>
       <h2>Hello SDC</h2>
+      <p>This is a group project of SDC</p>
+      <div>
+        <button>HELL YEAH</button>
+      </div>
     </div>
   )
 }
