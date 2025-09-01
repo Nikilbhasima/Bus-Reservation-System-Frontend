9e9ae2b3d13d3bc59244d5425336563b7683caa1
NIKIBHASIMA <nikilbhasima534@gmail.com>
2025-09-01 16:19:32 +0545

    adding sub branch

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,10 @@
 import "./App.css";
-import PrimaryButton from "./component/PrimaryButton";
-import SecondaryButton from "./component/SecondaryButton";
+import UserPage from "./user/UserPage";
 
 const App = () => {
   return (
     <>
-      <div>
-        <h2>Hello SDC</h2>
-        <br />
-        <label>Primary Button</label>
-        <PrimaryButton />
-        <br />
-        <label>Secondary Button</label>
-        <SecondaryButton />
-      </div>
-
-      <div className="text">HELLO WORLD</div>
-      <div>bye world</div>
+      <UserPage />
     </>
   );
 };
