2990ecf19b1e854c6bb86fdd63438ef92261ef35
NIKIBHASIMA <nikilbhasima534@gmail.com>
2025-09-05 16:00:29 +0545

    responsive navbar added

--- src/component/PrimaryButton.jsx
+++ src/component/PrimaryButton.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 
-function PrimaryButton({ name, width, handleSubmit }) {
+function PrimaryButton({
+  name,
+  width,
+  handleSubmit,
+  showBorder,
+  changeBackground,
+}) {
+  console.log(showBorder);
   return (
     <button
-      className={`py-[12px] px-[32px] rounded-[10px] bg-primary font-medium  border-none cursor-pointer text-white
- ${
-   width ? "w-fit" : "w-full"
- } hover:bg-white hover:text-primary hover:-translate-y-1 duration-300 transition-all ease-in`}
+      className={`  py-[12px] px-[24px]  sm:px-[12px] md:px-[24px] lg:px-[32px] rounded-[10px]  bg-primary font-medium cursor-pointer text-white
+  ${width ? "w-fit" : "w-full"}
+
+  ${changeBackground && " hover:bg-white hover:text-primary"}
+  
+  ${showBorder ? "border-2  hover:border-primary" : "border-none"}
+  hover:-translate-y-1 duration-300 transition-all ease-in`}
       onClick={handleSubmit}
     >
       {name}
--- src/component/SecondaryButton.jsx
+++ src/component/SecondaryButton.jsx
@@ -1,13 +1,22 @@
 import React from "react";
 
-function SecondaryButton({ name, width }) {
+function SecondaryButton({
+  name,
+  width,
+  handleSubmit,
+  changeBackground,
+  showBorder,
+}) {
   return (
     <button
-      className={`py-[12px] px-[32px] rounded-[10px] bg-white text-primary 
-  border border-transparent cursor-pointer font-medium
+      className={`py-[12px] px-[24px]  sm:px-[12px] md:px-[24px] lg:px-[32px] rounded-[10px] bg-white text-primary 
+ cursor-pointer font-medium border-2
   ${width ? "w-fit" : "w-full"}
-  hover:bg-primary hover:text-white hover:border-white hover:-translate-y-1
-  duration-300 transition-all ease-in`}
+  ${changeBackground && "hover:bg-primary hover:text-white hover:border-white"}
+  ${showBorder && "border-2 border-primary"}
+    hover:-translate-y-1
+  duration-300 transition-all ease-in `}
+      onClick={handleSubmit}
     >
       {name}
     </button>
