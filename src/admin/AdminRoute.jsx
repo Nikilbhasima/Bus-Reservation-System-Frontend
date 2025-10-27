283a1d87b028e3bbff87fc4142cb0c6874abdd6e
NIKIBHASIMA <nikilbhasima534@gmail.com>
2025-10-27 12:05:32 +0545

    improvement made in admin side

--- src/admin/AdminRoute.jsx
+++ src/admin/AdminRoute.jsx
@@ -8,14 +8,16 @@ import DriverDetail from "./driverDetail/DriverDetail";
 
 function AdminRoute() {
   return (
-    <Routes>
-      <Route path="/" element={<Dashboard />} />
-      <Route path="/booking" element={<Booking />} />
-      <Route path="/busDetail" element={<BusDetail />} />
-      <Route path="/driverDetail" element={<DriverDetail />} />
-      <Route path="/routes" element={<RootRoutes />} />
-      <Route path="*" element={<Dashboard />} />
-    </Routes>
+    <div className="flex-grow bg-[#ABABAB] p-[8px] sm:p-[16px] md:p-[24px] m-[16px] sm:m-x-[24px] md:m-x-[32px] rounded-[10px] overflow-y-auto">
+      <Routes>
+        <Route path="/" element={<Dashboard />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/busDetail" element={<BusDetail />} />
+        <Route path="/driverDetail" element={<DriverDetail />} />
+        <Route path="/routes" element={<RootRoutes />} />
+        <Route path="*" element={<Dashboard />} />
+      </Routes>
+    </div>
   );
 }
 
