c0e778a94f049c4b829ca95a971e7449af5b9a9a
Saliv Maharjan <vermion00@gmail.com>
2025-11-04 18:46:04 +0545

    Update Bus Function Implemented

--- src/redux/agencySlice/busSlice/BusSlice.js
+++ src/redux/agencySlice/busSlice/BusSlice.js
@@ -1,5 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { addBus, getAllBus, getBusById } from "./busThunks";
+import { addBus, getAllBus, getBusById, updateBusDetail } from "./busThunks";
 
 const initialState = {
   bus: null,
@@ -9,51 +9,65 @@ const initialState = {
 };
 
 const busSlice = createSlice({
-    name: "bus",
-    initialState,
-    reducers: {},
-    extraReducers: (builder) => {
-        builder
-        .addCase(addBus.pending, (state) => {
-            state.loading = true;
-            state.error = null;
-        })
-        .addCase(addBus.fulfilled, (state, action) =>{
-            state.loading = false;
-            state.error = null;
-            state.bus = action.payload;
-        })
-        .addCase(addBus.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload?.message || "Failed to Add the Bus";
-        })
-        .addCase(getAllBus.pending, (state) => {
-            state.loading = true;
-            state.error = null;
-        })
-        .addCase(getAllBus.fulfilled, (state, action) =>{
-            state.loading = false;
-            state.error = null;
-            state.bus = action.payload;
-        })
-        .addCase(getAllBus.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload?.message || "Failed to Get All Bus Details";
-        })
-        .addCase(getBusById.pending, (state) => {
-            state.loading = true;
-            state.error = null;
-        })
-        .addCase(getBusById.fulfilled, (state, action) =>{
-            state.loading = false;
-            state.error = null;
-            state.bus = action.payload;
-        })
-        .addCase(getBusById.rejected, (state, action) => {
-            state.loading = false;
-            state.error = action.payload?.message || "Failed to Get Bus Details";
-        })
-    }
-})
+  name: "bus",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(addBus.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addBus.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.bus = action.payload;
+      })
+      .addCase(addBus.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload?.message || "Failed to Add the Bus";
+      })
+      .addCase(getAllBus.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getAllBus.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.bus = action.payload;
+      })
+      .addCase(getAllBus.rejected, (state, action) => {
+        state.loading = false;
+        state.error =
+          action.payload?.message || "Failed to Get All Bus Details";
+      })
+      .addCase(getBusById.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(getBusById.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.bus = action.payload;
+      })
+      .addCase(getBusById.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload?.message || "Failed to Get Bus Details";
+      })
+      .addCase(updateBusDetail.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(updateBusDetail.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = null;
+        state.bus = action.payload;
+      })
+      .addCase(updateBusDetail.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload?.message || "Failed to Update Bus Details";
+      });
+  },
+});
 
 export default busSlice.reducer;
