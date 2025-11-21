8f1e03f831f983acd3ea2086db5607fde0bb98d6
Saliv Maharjan <vermion00@gmail.com>
2025-11-21 14:52:54 +0545

    Contact API Integration

---
+++ src/redux/userSlice/contactSlice/ContactSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { sendMessage } from "./ContactThunks";
+
+const initialState = {
+  user: null,
+  loading: false,
+  error: null,
+  success: false,
+};
+
+const contactSlice = createSlice({
+  name: "contactUs",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(sendMessage.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(sendMessage.fulfilled, (state, action) => {
+        state.loading = false;
+        state.success = true;
+        state.error = null;
+        state.bus = action?.payload;
+      })
+      .addCase(sendMessage.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload?.message || "Failed to send Message";
+      });
+  },
+});
+
+export default contactSlice.reducer;
